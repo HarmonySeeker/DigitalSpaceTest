9894adbec670e6dd6622015f249e02bc031cff78
HarmonySeeker <43490370+HarmonySeeker@users.noreply.github.com>
2024-10-29 07:32:38 +0700

    added camera manager; new input controls
    jumping, turning, etc

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,30 +6,48 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
+    #region Variables: Movement
+
     private Vector2 _input;
     private CharacterController _characterController;
     private Vector3 _direction;
-
-    [SerializeField] private float smoothTime = 0.05f;
-    private float _currentVelocity;
-
     [SerializeField] private float speed;
+
+    #endregion
+
+    #region Variables: Rotation
+
+    [SerializeField] private float rotationSpeed = 500f;
+    private Camera _mainCamera;
+
+    #endregion
+
+    #region Variables: Gravity
 
     private float _gravity = -9.81f;
     [SerializeField] private float gravityMultiplier = 3.0f;
     private float _velocity;
 
-    [SerializeField] private float jumpPower;
+    #endregion
 
+    #region Variables: Jumping
+    
+    [SerializeField] private float jumpPower;
+    private int _numberOfJumps;
+    [SerializeField] private int maxNumJumps = 2;
+
+    #endregion
+    
     private void Awake()
     {
         _characterController = GetComponent<CharacterController>();
+        _mainCamera = Camera.main;
     }
 
     private void Update()
     {
-        ApplyGravity();
         ApplyRotation();
+        ApplyGravity();
         ApplyMovement();
     }
 
@@ -40,9 +58,10 @@ public class PlayerController : MonoBehaviour
             return;
         }
 
-        float targetAngle = Mathf.Atan2(_direction.x, _direction.z) * Mathf.Rad2Deg;
-        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _currentVelocity, smoothTime);
-        transform.rotation = Quaternion.Euler(0.0f, angle, 0.0f);
+        _direction = Quaternion.Euler(0.0f, _mainCamera.transform.eulerAngles.y, 0.0f) * new Vector3 (_input.x, 0.0f, _input.y);
+        var targetRotation = Quaternion.LookRotation(_direction, Vector3.up);
+
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
     }
 
     private void ApplyMovement()
@@ -73,9 +92,19 @@ public class PlayerController : MonoBehaviour
     public void Jump(InputAction.CallbackContext context)
     {
         if (!context.started) return;
-        if (!IsGrounded()) return;
+        if (!IsGrounded() && _numberOfJumps >= maxNumJumps) return;
+        if (_numberOfJumps == 0) StartCoroutine(WaitForLanding());
 
-        _velocity += jumpPower;
+        _numberOfJumps++;
+        _velocity = jumpPower;
+    }
+
+    private IEnumerator WaitForLanding()
+    {
+        yield return new WaitUntil(() => !IsGrounded());
+        yield return new WaitUntil(IsGrounded);
+
+        _numberOfJumps = 0;
     }
 
     private bool IsGrounded() => _characterController.isGrounded;
